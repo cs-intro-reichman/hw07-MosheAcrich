f4b848b725402528c9a46ac787a806c99c4e7dfe
Moshe Acrich <moshe.acrich@post.runi.ac.il>
2026-01-02 11:56:27 +0100

    Try 1

--- Palindrome.java
+++ Palindrome.java
@@ -7,7 +7,13 @@ public class Palindrome {
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
+		if(s.length() <= 1){
+			return true;
+		}
+		if (s.charAt(0) != s.charAt(s.length()-1))´{
+			return false;
+		} 
 		//// Replace the followaing statement with your code
-		return false;
+		return isPalindrome(s.substring(1, s.length()-1));
     }
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -8,11 +8,38 @@ public class Sierpinski {
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
 		//// Replace this comment with your code
-	}
+
+        double x1 = 0.0;
+		double y1 = 0.0;
+        double x2 = 1.0; 
+		double y2 = 0.0;
+        double x3 = 0.5; 
+		double y3 = Math.sqrt( 3 / 4);
+
+        sierpinski(n, x1, x2, x3, y1, y2, y3);
+    }
+	
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
 		//// Replace this comment with your code
+		/// I do the basecase first, then i search for the halfpoint of every side in the triangle adn with the recursivity i do the 3 smaller triangels
+		if (n == 0){
+        	return;
+		}
+
+        StdDraw.line(x1, y1, x2, y2);
+        StdDraw.line(x2, y2, x3, y3);
+        StdDraw.line(x3, y3, x1, y1);
+		double x1HALF = (x1 + x2)/ 2;
+        double y1HALF = (y1 + y2)/ 2;
+        double x2HALF = (x2 + x3)/ 2;
+        double y2HALF = (y2 + y3)/ 2;
+        double x3HALF = (x3 + x1)/ 2;
+        double y3HALF = (y3 + y1)/ 2;
+		sierpinski(n - 1, x1, x1HALF, x3HALF, y1, y1HALF, y3HALF); 
+        sierpinski(n - 1, x1HALF, x2, x2HALF, y1HALF, y2, y2HALF); 
+        sierpinski(n - 1, x2HALF, x3, x3HALF, y2HALF, y3, y3HALF);
 	}
 }
